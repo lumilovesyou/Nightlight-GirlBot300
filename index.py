a80068fa19f4632ef3cfb7a91e769cabc82d693f
mewo <177341604+lumilovesyou@users.noreply.github.com>
2026-04-17 10:32:02 -0500

    Remove Old Tokens & Create Posts

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from dotenv import load_dotenv
+import mimetypes
 import os
 import platform
 import random
@@ -11,16 +12,58 @@ load_dotenv()
 
 # Init globals
 SITE = "https://nightlightapp.net/"
-API_POINT = f"{SITE}nlapi/user"
+API_POINT = f"{SITE}nlapi/"
 SESSION = requests.Session()
 
-# Testing username
-USERNAME = "felisaraneae"
+USERNAME = os.getenv("USERNAME")
 
 # Get token
-SESSION.post(f"{SITE}account/login", data={"loginusername": USERNAME, "loginpassword": os.getenv("PASSWORD")})
-print(SESSION.cookies.get_dict())
+try:
+    print("Attempting to get key")
+    SESSION.post(f"{SITE}account/login", data={"loginusername": USERNAME, "loginpassword": os.getenv("PASSWORD")})
+    if (SESSION.cookies.get_dict()["username"] != USERNAME):
+        print("Failed to get token!")
+        exit()
+except:
+    print("Failed to get token!")
 
-# Test using token
-request = SESSION.get(f"{API_POINT}?action=getUnreadNotifications")
-print(request.text)
+# Invalid the previous token (probably a really gross way to do this but I couldn't find an endpoint to grab tokens from)
+try:
+    print("Attempting to remove tokens")
+    grabTokens = SESSION.get(f"{SITE}u/{USERNAME}").text
+    grabTokens = json.loads(grabTokens.split("\"loginTokens\":")[1].split("});document.getElementById('notificationdot-notifications')")[0])
+    for i in grabTokens:
+        if (i["device"] == "python-requests/2.25.1" and not i["active"]):
+            # Remove old tokens
+            SESSION.post(f"{API_POINT}user", params={"action": "invalidateToken"}, json={"tokenId": i["id"]})
+except:
+    print("Failed to remove old tokens")
+    
+print("Ready Mr. Stark!")
+    
+def createPost(text, category="other", filePath=None):
+    files = {}
+    if (filePath and os.path.exists(filePath)):
+        # Don't really need more than one image right now so... ~~~~~~~~~~
+        files = {
+            "file0": (
+                os.path.basename(filePath),
+                open(filePath, "rb"),
+                mimetypes.guess_type(filePath)[0] or "application/octet-stream"
+            )
+        }
+        
+    response = SESSION.post(f"{API_POINT}post",
+        data={
+            "content": text,
+            "category": category,
+            "visibility": 0,
+            "views": 2
+        },
+        files=files
+    )
+    print(response.text)
+    print(response.status_code)
+    print(f"Posted \"{text}\"!")
+
+createPost("Automation test, ignore me!", "other", "/Users/felisaraneae/Downloads/celeste.webp")
